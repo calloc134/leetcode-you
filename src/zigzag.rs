6643507c6e0fed60db5d655916e842c3fac2918b
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-10-04 23:09:23 +0900

    ジグザグ100%

--- src/zigzag.rs
+++ src/zigzag.rs
@@ -1,6 +1,43 @@
 pub struct Solution;
 impl Solution {
-    pub fn convert(s: String, num_rows: i32) -> String {}
+    pub fn convert(s: String, num_rows: i32) -> String {
+        // もしnum_rowsが1以下の場合はそのまま返す
+        if num_rows <= 1 {
+            return s;
+        }
+
+        // modを行う数字について求める
+        let mod_number = (num_rows - 1) * 2;
+
+        // 与えられた文字列について
+        // enumerateを使ってインデックスを含んだタプルにしながらcharベクタに変換
+        let s_char_with_index = s.chars().enumerate().collect::<Vec<(usize, char)>>();
+
+        // 行数分のベクタを用意
+        let mut rows = vec![String::new(); num_rows as usize];
+
+        // 与えられた文字列についてループ
+        for (i, c) in s_char_with_index {
+            // mod_numberで割った余りを求める
+            let remainder = i % mod_number as usize;
+
+            // まずあまりが中央より小さい場合
+            if remainder < num_rows as usize {
+                // そのまま行に追加
+                rows[remainder].push(c);
+            } else {
+                // あまりが中央より大きい場合
+                // 中央からの差分を求める
+                let diff = remainder - num_rows as usize;
+
+                // 中央からの差分を引いた行に追加
+                rows[num_rows as usize - 2 - diff].push(c);
+            }
+        }
+
+        // 行数分の文字列を結合して返す
+        rows.join("")
+    }
 }
 
 #[cfg(test)]
