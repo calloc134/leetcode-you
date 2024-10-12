2a1a3c362185d8d3d4affeda04b82ede026c3b7f
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-10-12 15:54:33 +0900

    two sum解答

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -1,34 +1,20 @@
 pub struct Solution;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        // インデックスをつけてタプルにする
-        // index, valueの順番にする
-        let nums_with_index: Vec<(usize, i32)> = nums.into_iter().enumerate().collect();
+        // ハッシュマップを作成
+        // キー→数値、値→インデックス
+        let mut hash_map: std::collections::HashMap<i32, i32> = std::collections::HashMap::new();
 
-        // target以上の値を持つ要素を取り除く
-        let nums_with_index: Vec<(usize, i32)> = nums_with_index
-            .into_iter()
-            .filter(|(_, num)| *num < target)
-            .collect();
-
-        // target - xにした
-
-        // target - xにした配列を作る
-        // let target_minus_x: Vec<(usize, i32)> = nums_with_index
-        //     .iter()
-        //     .map(|(index, num)| (*index, target - num))
-        //     .collect();
-
-        // // target - xにした配列と元の配列を比較して、一致するものを返却
-        // for (index, num) in target_minus_x {
-        //     for (i, n) in nums_with_index.iter() {
-        //         if num == *n && index != *i {
-        //             return vec![index as i32, *i as i32];
-        //         }
-        //     }
-        // }
-
-        return vec![];
+        for (i, num) in nums.iter().enumerate() {
+            // ターゲットから数値を引いた値がハッシュマップに存在するか
+            let diff = target - num;
+            if let Some(&index) = hash_map.get(&diff) {
+                return vec![index, i as i32];
+            }
+            // 存在しない場合はハッシュマップに数値とインデックスを追加
+            hash_map.insert(num.clone(), i as i32);
+        }
+        vec![]
     }
 }
 
@@ -36,10 +22,34 @@ impl Solution {
 mod tests {
     use super::*;
     #[test]
-    fn test_two_sum() {
+    fn test_two_sum_1() {
         let nums = vec![2, 7, 11, 15];
         let target = 9;
         let result = Solution::two_sum(nums, target);
         assert_eq!(result, vec![0, 1]);
+    }
+
+    #[test]
+    fn test_two_sum_2() {
+        let nums = vec![3, 2, 4];
+        let target = 6;
+        let result = Solution::two_sum(nums, target);
+        assert_eq!(result, vec![1, 2]);
+    }
+
+    #[test]
+    fn test_two_sum_3() {
+        let nums = vec![3, 3];
+        let target = 6;
+        let result = Solution::two_sum(nums, target);
+        assert_eq!(result, vec![0, 1]);
+    }
+
+    #[test]
+    fn test_two_sum_4() {
+        let nums = vec![0, 4, 3, 0];
+        let target = 0;
+        let result = Solution::two_sum(nums, target);
+        assert_eq!(result, vec![0, 3]);
     }
 }
