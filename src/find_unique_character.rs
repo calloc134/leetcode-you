4f97956c1bb9f9dcb6ff9554f5b5860b8427fded
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-10-14 16:58:31 +0900

    実装完了

--- src/find_unique_character.rs
+++ src/find_unique_character.rs
@@ -1,7 +1,36 @@
 pub struct Solution;
 impl Solution {
     pub fn first_uniq_char(s: String) -> i32 {
-        return 0;
+        // 出現したインデックスと回数を保存するためのハッシュマップ
+        // キー→記号、値→(インデックス, 回数)のタプル
+        let mut index_map = std::collections::HashMap::new();
+
+        for (i, c) in s.chars().enumerate() {
+            // キーが存在しない場合は、(i, 0)を挿入
+            if !index_map.contains_key(&c) {
+                index_map.insert(c, (i, 1));
+            }
+            // キーが存在する場合は、回数をインクリメント
+            else {
+                let (_, count) = index_map.get_mut(&c).unwrap();
+                *count += 1;
+            }
+        }
+
+        // 回数が1のものを洗い出して、最小のインデックスを返す
+        let mut min_index = s.len();
+
+        for (_, (i, count)) in index_map.iter() {
+            if *count == 1 && *i < min_index {
+                min_index = *i;
+            }
+        }
+
+        // 回数が1のものがない場合は-1を返す
+        if min_index == s.len() {
+            return -1;
+        }
+        min_index as i32
     }
 }
 
