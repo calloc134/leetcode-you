40df34036acfb5c57b5a7b4fb1fcf542820025c3
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-10-14 16:35:46 +0900

    解答

--- src/romen_to_integer.rs
+++ src/romen_to_integer.rs
@@ -1,7 +1,44 @@
+use std::collections::HashMap;
+
+
 pub struct Solution;
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        return 0;
+        let s_char = s.chars().collect::<Vec<char>>();
+
+        // ローマ字の対応表のハッシュマップを作成
+        let roman_map: HashMap<char, i32> = [
+            ('I', 1),
+            ('V', 5),
+            ('X', 10),
+            ('L', 50),
+            ('C', 100),
+            ('D', 500),
+            ('M', 1000),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let s_roman = s_char
+            .iter()
+            .map(|&c| roman_map.get(&c).unwrap())
+            .collect::<Vec<&i32>>();
+
+        let mut result = 0;
+
+        // 末尾から見ていく
+        for count in (0..s_roman.len()).rev() {
+            // もし最初でなく、現在の数字が次の数字より小さい場合は、引く
+            // if s_roman[count] < s_roman[count + 1] {
+            if count < s_roman.len() - 1 && s_roman[count] < s_roman[count + 1] {
+                result -= s_roman[count];
+            } else {
+                result += s_roman[count];
+            }
+        }
+
+        result
     }
 }
 
