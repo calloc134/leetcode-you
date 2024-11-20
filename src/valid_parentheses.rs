4b73be11532cf012e818dcc3b059a9d7d420fb4e
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-11-20 10:25:23 +0900

    実装してみた 閉じ括弧いきなり来るとエラー

--- src/valid_parentheses.rs
+++ src/valid_parentheses.rs
@@ -1,7 +1,37 @@
 pub struct Solution;
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        return false;
+        let bracket_hashset = std::collections::HashSet::from(['(', ')', '[', ']', '{', '}']);
+
+        let bracket_start_hashset = std::collections::HashSet::from(['(', '[', '{']);
+        let bracket_end_hashset = std::collections::HashSet::from([')', ']', '}']);
+
+        let bracket_pair_hashmap =
+            std::collections::HashMap::from([('(', ')'), ('[', ']'), ('{', '}')]);
+
+        let mut stack: Vec<char> = Vec::new();
+
+        let s_char = s.chars().collect::<Vec<char>>();
+
+        for char in s_char {
+            if !bracket_hashset.contains(&char) {
+                continue;
+            }
+
+            if bracket_end_hashset.contains(&char) {
+                if let Some(last) = stack.pop() {
+                    if last != char {
+                        return false;
+                    }
+                }
+            }
+
+            if bracket_start_hashset.contains(&char) {
+                stack.push(*bracket_pair_hashmap.get(&char).unwrap());
+            }
+        }
+
+        return stack.is_empty();
     }
 }
 
